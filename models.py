243fd83e2574b27db4954e836846f7e90ce84fb6
abalsaffar <136919974+abalsaffar@users.noreply.github.com>
2023-07-02 17:57:05 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,34 +1,36 @@
-# Import necessary modules from flask_sqlalchemy
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Boolean, Column, Integer, String
 
-# Initialize a SQLAlchemy object
-db = SQLAlchemy()
-
-# Define the User model
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # Unique ID for each user
-    name = db.Column(db.String(255), nullable=False)  # User's name
-    email = db.Column(db.String(255), nullable=False, unique=True)  # User's email, must be unique
-    password = db.Column(db.String(255), nullable=False)  # User's hashed password
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    email = Column(String(255), unique=True)
+    password = Column(String(255))
+    verified = Column(Boolean, default=False)
+    code = Column(String(255))
 
-# Define the Category model
+    def __repr__(self):
+        return '<User id={id}, name={name}, email={email}, verified={verified}>'.format(
+            id=self.id,
+            name=self.name,
+            email=self.email,
+            verified=self.verified,
+        )
+
 class Category(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # Unique ID for each category
-    name = db.Column(db.String(255), nullable=False, unique=True)  # Category name, must be unique
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
 
-# Define the SubCategory model
 class SubCategory(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # Unique ID for each subcategory
-    name = db.Column(db.String(255), nullable=False)  # Subcategory name
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)  # Foreign key linking subcategory to its category
-    category = db.relationship('Category', backref=db.backref('subcategories', lazy=True))  # Relationship between Category and SubCategory models
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    category_id = Column(Integer, db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship('Category', backref=db.backref('subcategories', lazy=True))
 
-# Define the Content model
 class Content(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # Unique ID for each content item
-    title = db.Column(db.String(255), nullable=False)  # Content title
-    description = db.Column(db.Text, nullable=False)  # Content description
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Foreign key linking content to its author (a user)
-    author = db.relationship('User', backref=db.backref('contents', lazy=True))  # Relationship between User and Content models
-    subcategory_id = db.Column(db.Integer, db.ForeignKey('sub_category.id'), nullable=False)  # Foreign key linking content to its subcategory
-    subcategory = db.relationship('SubCategory', backref=db.backref('contents', lazy=True))  # Relationship between SubCategory and Content models
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255), nullable=False)
+    description = Column(Text, nullable=False)
+    user_id = Column(Integer, db.ForeignKey('user.id'), nullable=False)
+    author = db.relationship('User', backref=db.backref('contents', lazy=True))
+    subcategory_id = Column(Integer, db.ForeignKey('subcategory.id'), nullable=False)
+    subcategory = db.relationship('SubCategory', backref=db.backref('contents', lazy=True))
